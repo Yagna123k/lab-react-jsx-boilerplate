a3a0f1e37671c8ce4c16d73830436c2071e8cb9f
Yagna123k <yagna82kb@gmail.com>
2023-12-06 17:41:46 +0530

    Assignment Completed

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,25 @@ const imageData = ()=>{
 }
 
 function App() {
-  // code here
+
+  let img_arr = imageData()
+  
+  return (
+    <>
+    <h1>Kalvium Gallery</h1>
+    <div className='parent_div'>
+      {/* <img src={img_arr[0].img} className='image_div' alt="" />
+      <img src={img_arr[1].img} className='image_div' alt="" />
+      <img src={img_arr[2].img} className='image_div' alt="" />
+      <img src={img_arr[3].img} className='image_div' alt="" /> */}
+      {img_arr.map(item=>{
+        return (
+          <img src={item.img} alt='elphant' className='image_div'></img>
+        )
+      })}
+    </div>
+    </>
+  )
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -27,4 +27,23 @@ export default class AppClass extends Component{
   }
 
   // code here
+  render(){
+    let img_arr = this.imageData()
+    return (
+      <>
+      <h1>Kalvium Gallery</h1>
+      <div className='parent_div'>
+        {/* <img src={img_arr[0].img} className='image_div' alt="" />
+        <img src={img_arr[1].img} className='image_div' alt="" />
+        <img src={img_arr[2].img} className='image_div' alt="" />
+        <img src={img_arr[3].img} className='image_div' alt="" /> */}
+        {img_arr.map(item=>{
+          return (
+            <img src={item.img} alt='elphant' className='image_div'></img>
+          )
+        })}
+      </div>
+      </>
+    )
+  }
 }
